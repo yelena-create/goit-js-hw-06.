3dd46a2f4c594d2e10105b47d8f66df22f78fc57
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2022-02-03 22:23:07 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,36 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+
+const makeingredientsCard = ingredients => {
+  return ingredients.map(ingredient => {
+    const rowEl = document.createElement('li');
+    rowEl.classList.add('item');
+    rowEl.textContent = ingredient;
+    return rowEl;
+  });
+};
+const elements = makeingredientsCard(ingredients);
+
+ingredientsEl.append(...elements);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
