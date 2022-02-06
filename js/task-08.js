3ab90645a40b3bf880f6e11504276fb41b1f642c
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2022-02-06 15:52:36 +0200

    tasc 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,41 @@
+const refs = {
+    formRef: document.querySelector('.login-form'),
+    emailRef: document.querySelectorAll('.login-form input'),
+    ButtonSubmitRef:document.querySelector('.login-form>button'),
+}; 
+// console.log(refs.formRef);          //форма
+// console.log(refs.emailRef);
+// console.log(refs.emailRef[0]);      //емейл
+// console.log(refs.emailRef[1]);      // пароль
+// console.log(refs.ButtonSubmitRef);  //кнопка
+
+refs.formRef.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (refs.emailRef[0].value === "" || refs.emailRef[1].value === "") {
+        alert('Внимание! Все поля формы должны быть заполнены!');
+    } else { 
+        const formData = new FormData(event.currentTarget);
+    console.log(FormData);
+    formData.forEach((value, name) => {
+        console.log('value=', value, 'name=', name    );
+    });
+        refs.formRef.reset();
+    }
+    
+}
+function reset() { 
+    refs.emailRef.value = '';
+}
+
+
+
+
+
+
+
+
 
