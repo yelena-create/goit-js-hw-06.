dc8157fbb6ae7c0c0d26f5bf12cea841b88860c2
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2022-02-06 17:03:11 +0200

    tasc 8.1

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,18 @@
 const buttonRef = document.querySelector('.change-color');
-const backgroundColor = document.querySelector('.widget');
 const spanRef = document.querySelector('.color');
+
+const RandomHexColor = getRandomHexColor();
 
 buttonRef.addEventListener('click', onBackgroundColorChange);
 
 function onBackgroundColorChange() { 
-    console.log('hello');
-  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  console.log('hello');
   spanRef.textContent = `${getRandomHexColor()}`;
+  document.body.style.backgroundColor = spanRef.textContent;
+  console.log(document.body.style.backgroundColor);
+  console.log(spanRef.textContent);
   };
     
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-// console.log(Math.floor(Math.random() * 16777215).toString(16))
+  }
