ced6c45a4fd0e492a9266356a6e84af1e88b7617
Yakushko Yelena <82969672+yelena-create@users.noreply.github.com>
2022-02-06 17:26:27 +0200

    tasc 7.2

--- js/task-07.js
+++ js/task-07.js
@@ -1,16 +1,25 @@
-const inputSizeControl = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
+const inputSizeControlRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
 
-inputSizeControl.addEventListener('click', onTextFontChange);
+inputSizeControlRef.addEventListener('input', onTextFontChange);
 
 function onTextFontChange(event) {
-    text.style.fontSize = event.currentTarget.value + "px";
-}
+//    const fontSize = Number('${event.currentTarget.value} px');    
+    textRef.style.fontSize = `${event.currentTarget.value}px`;
+
+console.log(event.currentTarget.value);
+ };
 
 
 
+// const inputSizeControl = document.querySelector('#font-size-control');
+// const text = document.querySelector('#text');
 
+// inputSizeControl.addEventListener('click', onTextFontChange);
 
-
+// function onTextFontChange(event) {
+//     text.style.fontSize = event.currentTarget.value + "px";
+//конкантенация строк?
+// }
 
 
--- js/task-09.js
+++ js/task-09.js
@@ -9,9 +9,9 @@ function onBackgroundColorChange() {
   console.log('hello');
   spanRef.textContent = `${getRandomHexColor()}`;
   document.body.style.backgroundColor = spanRef.textContent;
-  console.log(document.body.style.backgroundColor);
-  console.log(spanRef.textContent);
-  };
+  // console.log(document.body.style.backgroundColor);
+  // console.log(spanRef.textContent);
+  }
     
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
